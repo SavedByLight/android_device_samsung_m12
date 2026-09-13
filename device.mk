ffc07527ddf97f60ea384e179f4eafdec43ec7cd
Jamie Falconer <mrfluffyoven@gmail.com>
2026-09-13 08:56:16 +0100

    rework: treewide
    Signed-off-by: Jamie Falconer <mrfluffyoven@gmail.com>

--- device.mk
+++ device.mk
@@ -6,3 +6,24 @@
 #
 
 LOCAL_PATH := device/samsung/m12
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# sideload
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
